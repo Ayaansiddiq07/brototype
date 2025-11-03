5319351dd879b59eb2cd58289f31814df09fe6ed
Ayaansiddiq07 <ayaansiddia@gamil.com>
2025-11-03 21:21:57 +0530

    gained knowledge in inheritance and polymorphism in java

---
+++ polymorphism.java
@@ -0,0 +1,27 @@
+//lets understand polymorphism in java
+
+class Animal {
+    void sound() {
+        System.out.println("Animal makes a sound");
+    }
+}
+class Dog extends Animal {
+    void sound() {
+        System.out.println("Dog barks");
+    }
+}
+class Cat extends Animal {
+    void sound() {
+        System.out.println("Cat meows");
+    }
+}
+public class polymorphism {
+    public static void main(String args[]) {
+        Animal myanimal=new Animal();
+        Animal mydog=new Dog();
+        Animal mycat=new Cat();
+        myanimal.sound();
+        mydog.sound();
+        mycat.sound();
+    }
+}
